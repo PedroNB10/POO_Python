90fe2eded1f6af425bcf6889c41c306866a94410
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-10-01 15:48:17 -0300

    ajustes

--- testeconta.py
+++ testeconta.py
@@ -1,24 +1,23 @@
 from abc import ABC,abstractclassmethod
 from datetime import datetime
+import random
 
 def formatarData(data):
     return data.strftime("%d/%m/%Y %H:%M:%S")
 
 class Transacao:
-    def __init__(self,conta,valor,descricao, nova_transacao = False):
+    def __init__(self,conta,valor, nova_transacao = False):
         self.__conta = conta
         self.__valor = valor
-        self.__descricao = descricao
+        self.__descricao = "Crédito" if valor > 0 else "Débito"
         self.__data = formatarData(datetime.now())
 
-        # conta.lista_transacoes.append(self)  
-        
         if nova_transacao == False:
             if valor < 0:
-                conta.retirada((-1) * valor, descricao, self)
+                conta.retirada((-1) * valor, self)
   
             elif valor > 0:
-                conta.deposito(valor, descricao, self)
+                conta.deposito(valor, self)
                 
     @property
     def data(self):
@@ -40,10 +39,10 @@ class Transacao:
 
 class Conta(ABC):
     
-    def __init__(self,nome,numero,saldo):
+    def __init__(self,nome):
         self.__nome = nome
-        self.__numero = numero
-        self.__saldo = saldo
+        self.__numero = str(random.randint(10**12, 10**13 - 1))
+        self.__saldo = 0
         self.__lista_transacoes = []
         
     @property
@@ -70,61 +69,79 @@ class Conta(ABC):
     def lista_transacoes(self,valor):
         self.__lista_transacoes = valor
         
-    def retirada(self,valor,descricao, transacao = None):
-        if valor <= self.__saldo and transacao is None:
-            self.__saldo -= valor
-            transacao = Transacao(self,-valor,descricao,True)
-            self.lista_transacoes.append(transacao)
-          
-        elif valor <= self.__saldo and transacao is not None:
-            self.__saldo -= valor
-            self.lista_transacoes.append(transacao)
-       
+    def retirada(self,valor,transacao = None):
+        if valor < 0:
+            print("Valor Inválido")
+        
+        elif self.saldo - valor < 0:
+            print("Saldo Insuficiente!")
+
         else:
-            print("Saldo Insuficiente")
+            if valor <= self.__saldo and transacao is None:
+                self.__saldo -= valor
+                transacao = Transacao(self,-valor,True)
+                self.lista_transacoes.append(transacao)
             
-    def deposito(self,valor,descricao, transacao = None):
-        if  transacao is None:
-            self.__saldo += valor
-            transacao = Transacao(self,valor,descricao,True)
+            elif valor <= self.__saldo and transacao is not None:
+                self.__saldo -= valor
+                self.lista_transacoes.append(transacao)
+        
+            else:
+                print("Saldo Insuficiente")
             
-        elif transacao is not None:
-            self.__saldo += valor
-          
+    def deposito(self,valor,transacao = None):
+        if valor < 0:
+            print("Valor Inválido")
+        else:
+            if  transacao is None:
+                self.__saldo += valor
+                transacao = Transacao(self,valor,True)
+                self.lista_transacoes.append(transacao)
+            elif transacao is not None:
+                self.__saldo += valor
+                self.lista_transacoes.append(transacao)
 
     @abstractclassmethod
     def imprimirExtrato(self):
-
-        print()
+        pass
 
 class ContaComum(Conta):
-    def __init__(self,nome,numero,saldo):
-        super().__init__(nome,numero,saldo)
+    def __init__(self,nome):
+        super().__init__(nome)
     
     def imprimirExtrato(self):
-        print(f"Conta Comum:\nNome: {self.nome}  Número: {self.numero}  Saldo: {self.saldo}\n")
+        print(f"Conta Corrente Comum:\nNome: {self.nome}  Número: {self.numero}  Saldo: {self.saldo}\n")
         if len(self.lista_transacoes) > 0:
             print("Transações: ")
             for i in self.lista_transacoes:
                 sinal = "+" if i.valor > 0 else ""
-                print(f"Data: {i.data} Valor: R$ {sinal}{i.valor} Descrição: {i.descricao}")
-                print(" ")
+                print(f"\tData: {i.data} Valor: R$ {sinal}{i.valor} Descrição: {i.descricao}")
+
 
 
 class ContaPoupanca(Conta):
     
-    def __init__(self,nome,numero,saldo):
-        super().__init__(nome,numero,saldo)
+    def __init__(self,nome):
+        super().__init__(nome)
+        # para pegar o dia de criacao => formatarData(datetime.now())
         self.__aniversario = datetime.now().day
 
     @property
     def aniversario(self):
         return self.__aniversario
     
+    def imprimirExtrato(self):
+        print(f"Conta Poupança:\nNome: {self.nome}  Número: {self.numero}  Saldo: {self.saldo} Dia do Aniversário: {self.__aniversario}\n")
+        if len(self.lista_transacoes) > 0:
+            print("Transações: ")
+            for i in self.lista_transacoes:
+                sinal = "+" if i.valor > 0 else ""
+                print(f"\tData: {i.data} Valor: R$ {sinal}{i.valor} Descrição: {i.descricao}")
+    
 class ContaComLimite(Conta):
     
-    def __init__(self,nome,numero,saldo,limite):
-        super().__init__(nome,numero,saldo)
+    def __init__(self,nome,limite):
+        super().__init__(nome)
         self.__limite = limite
         self.__limite_inicial = limite
         
@@ -137,95 +154,106 @@ class ContaComLimite(Conta):
         self.__limite = novo_limite
         
 
-    def retirada(self,valor,descricao, transacao = None):
-        # ajuste do valor :
-        valor = - valor if valor < 0 else valor
-        if transacao is None:
-   
-            if self.saldo -valor >= 0:
-                self.saldo -= valor
-                transacao = Transacao(self,valor,descricao,True)
-                self.lista_transacoes.append(transacao)
+    def retirada(self,valor, transacao = None):
+        if valor < 0:
+            print("Valor Inválido")
 
-            elif self.saldo > 0 and self.saldo -valor  < 0  and self.__limite - (valor - self.saldo) >= 0:
-                valor_restante = valor - self.saldo
-                self.saldo -= valor 
-                self.__limite -= valor_restante
-                transacao = Transacao(self,valor,descricao,True)
-                self.lista_transacoes.append(transacao)
-
-            elif self.saldo < 0 and self.__limite > 0 and self.__limite < self.__limite_inicial and self.__limite - valor >= 0:
-                self.saldo -= valor
-                self.__limite -= valor
-                transacao = Transacao(self,valor,descricao,True)
-                self.lista_transacoes.append(transacao)         
-        else:   
-
-            if self.saldo -valor >= 0:
-                self.saldo -= valor
-                self.lista_transacoes.append(transacao)
-
-            elif self.saldo > 0 and self.saldo -valor  < 0  and self.__limite - (valor - self.saldo) >= 0:
-                valor_restante = valor - self.saldo
-                self.saldo -= valor 
-                self.__limite -= valor_restante
-                self.lista_transacoes.append(transacao)
-
-            elif self.saldo < 0 and self.__limite > 0 and self.__limite < self.__limite_inicial and self.__limite - valor >= 0:
-                self.saldo -= valor
-                self.__limite -= valor
-                self.lista_transacoes.append(transacao)
-
-           
-            
-    def deposito(self,valor,descricao, transacao = None):
-        # ajuste do valor
-        valor = -valor if valor < 0 else valor
-        if transacao == None:
-            
-            if valor + self.__limite <= self.__limite_inicial:
-                self.saldo += valor
-                self.__limite += valor
-                transacao = Transacao(self,valor,descricao,True)
-                self.lista_transacoes.append(transacao)
-             
-            
-            elif valor + self.__limite > self.__limite_inicial:
-                self.__limite = self.__limite_inicial
-                self.saldo += valor
-                transacao = Transacao(self,valor,descricao,True)
-                self.lista_transacoes.append(transacao)
-               
         else:
-            if valor + self.__limite <= self.__limite_inicial:
-                self.saldo += valor
-                self.__limite += valor
-                self.lista_transacoes.append(transacao)
-              
-            elif valor + self.__limite > self.__limite_inicial:
-                self.__limite = self.__limite_inicial
-                self.saldo += valor
-                self.lista_transacoes.append(transacao)            
+            if transacao is None:
+    
+                if self.saldo -valor >= 0:
+                    self.saldo -= valor
+                    transacao = Transacao(self,valor,True)
+                    self.lista_transacoes.append(transacao)
+
+                elif self.saldo > 0 and self.saldo -valor  < 0  and self.__limite - (valor - self.saldo) >= 0:
+                    valor_restante = valor - self.saldo
+                    self.saldo -= valor 
+                    self.__limite -= valor_restante
+                    transacao = Transacao(self,valor,True)
+                    self.lista_transacoes.append(transacao)
+
+                elif self.saldo < 0 and self.__limite > 0 and self.__limite < self.__limite_inicial and self.__limite - valor >= 0:
+                    self.saldo -= valor
+                    self.__limite -= valor
+                    transacao = Transacao(self,valor,True)
+                    self.lista_transacoes.append(transacao)  
+                else:
+                    print("Saldo Insuficiente")       
+            else:   
+
+                if self.saldo -valor >= 0:
+                    self.saldo -= valor
+                    self.lista_transacoes.append(transacao)
+
+                elif self.saldo > 0 and self.saldo -valor  < 0  and self.__limite - (valor - self.saldo) >= 0:
+                    valor_restante = valor - self.saldo
+                    self.saldo -= valor 
+                    self.__limite -= valor_restante
+                    self.lista_transacoes.append(transacao)
+
+                elif self.saldo < 0 and self.__limite > 0 and self.__limite < self.__limite_inicial and self.__limite - valor >= 0:
+                    self.saldo -= valor
+                    self.__limite -= valor
+                    self.lista_transacoes.append(transacao)
+                
+                else:
+                    print("Saldo Insuficiente")
+
+    def deposito(self,valor,transacao = None):
+        if valor < 0:
+            print("Valor Inválido")
+        else:
+            if transacao == None:
+                
+                if valor + self.__limite <= self.__limite_inicial:
+                    self.saldo += valor
+                    self.__limite += valor
+                    transacao = Transacao(self,valor,True)
+                    self.lista_transacoes.append(transacao)
+                
+                
+                elif valor + self.__limite > self.__limite_inicial:
+                    self.__limite = self.__limite_inicial
+                    self.saldo += valor
+                    transacao = Transacao(self,valor,True)
+                    self.lista_transacoes.append(transacao)
+                
+            else:
+                if valor + self.__limite <= self.__limite_inicial:
+                    self.saldo += valor
+                    self.__limite += valor
+                    self.lista_transacoes.append(transacao)
+                
+                elif valor + self.__limite > self.__limite_inicial:
+                    self.__limite = self.__limite_inicial
+                    self.saldo += valor
+                    self.lista_transacoes.append(transacao)            
               
                 
     def imprimirExtrato(self):
-        print(f"Conta Limite\nNome: {self.nome}  Número: {self.numero}  Saldo: {self.saldo}")
-        print(f"Limite: {self.__limite}\n")
+        print(f"Conta Corrente com Limite\nNome: {self.nome}  Número: {self.numero}  Saldo: {self.saldo} Limite: {self.__limite}\n")
         if len(self.lista_transacoes) > 0:
             print("Transações: ")
             for i in self.lista_transacoes:
                 sinal = "+" if i.valor > 0 else ""
-                print(f"Data: {i.data} Valor: R$ {sinal}{i.valor} Descrição: {i.descricao}")
-                print(" ")
+                print(f"\tData: {i.data} Valor: R$ {sinal}{i.valor} Descrição: {i.descricao}")
+
     
 if __name__ == "__main__":
-    conta = ContaComLimite("Maria",123,1000,1000)
+    conta = ContaComLimite("Maria",1500)
+    conta2 = ContaPoupanca("Bob")
+    conta3 = ContaComum("Joel")
     conta.imprimirExtrato()
-    trans = Transacao(conta,-800,"Retirada")
-    trans = Transacao(conta,100,"Deposito")
-    trans = Transacao(conta,-400,"Retirada")
-    trans = Transacao(conta,-400,"Retirada")
-    trans = Transacao(conta,-300,"Retirada")
-    trans = Transacao(conta,-200,"Retirada")
+    # conta2.imprimirExtrato()
+    # conta3.imprimirExtrato()
+    conta.deposito(1000)
 
+    trans = Transacao(conta,-800)
+    trans = Transacao(conta,-400)
+    trans = Transacao(conta,-400)
+    trans = Transacao(conta,-300)
+    trans = Transacao(conta,-200)
+    trans = Transacao(conta,-400)
+    trans = Transacao(conta,-1)
     conta.imprimirExtrato()
